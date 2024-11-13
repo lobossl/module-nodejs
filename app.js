09480d02d21c2c6b9dfe1c16f07b6581dce2ca57
Lobo <hagemo83@gmail.com>
2024-11-13 18:26:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const load = require("./logger")
+const db = require("./database")
+const express = require("express")
+const app = express()
 
-console.log( load )
+app.get("/:str",(req,res) =>{
+        const getStr = req.params.str
+
+        const add = db.add({
+                id: 1,
+                text: getStr
+        })
+
+        if(add){
+                res.send(getStr)
+        }
+        else{
+                res.send("error")
+        }
+})
+
+app.listen(3333)
